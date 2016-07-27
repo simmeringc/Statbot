39684276165fd4b18b1ad3ddff3b100225e4dd5b
Conner Simmering <connersimmering@gmail.com>
2016-07-27 17:46:57 -0600

    working-no-file-structure

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,12 @@
 import os
+from slackbot.bot import respond_to
 API_TOKEN = os.environ.get('SLACKBOT_API_TOKEN')
 ERROT_TO = "general"
 PLUGINS = [
     'slackbot.plugins',
 ]
 DEFAULT_REPLY = "Sorry but I didn't understand you!!"
+
+@respond_to('hola')
+def hola(message):
+    message.reply('yolloha')
