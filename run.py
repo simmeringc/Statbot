9750118f036ed30b1042d7358c20f4e9754df7df
Boraz <Maxson.Daniel@gmail.com>
2016-07-27 18:13:35 -0600

    StatBot file added to put replys

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
+import StatBot
 from slackbot.bot import Bot
 from slackbot import settings
-from slackbot.bot import respond_to
 
 def main():
-    @respond_to('hola')
-    def hola(message):
-        message.reply('yolloha')
+
     bot = Bot()
     bot.run()
 
